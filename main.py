f8537c625827f59b0ad167571968e7fffa64f2c8
Gavin Lee <46236119+GavinSLee@users.noreply.github.com>
2020-06-26 16:04:50 -0700

    More changes

--- main.py
+++ main.py
@@ -55,6 +55,36 @@ class MatchData:
 
         return total_match_history
 
+    def cleanMatchHistory(self, match_history):
+        """ Cleans match history by removing all games that are not on Summoner's Rift (i.e. removing games that are all ARAM).
+
+        :type match_history: List[Dict]
+        :rtype: List[Dict] 
+        """  
+
+        cleaned_match_history = [] 
+        counter = 0
+        for match in match_history:
+            matchId = str(match['gameId'])
+            curr_match_stats = self.getGameStats(matchId)  
+            curr_game_mode = curr_match_stats['gameMode']
+
+            print(matchId) 
+            print(curr_game_mode)
+            print("Counter: " + str(counter) + "\n")
+            counter += 1
+            time.sleep(1) 
+
+            if curr_game_mode != "CLASSIC":
+                continue 
+            else: 
+                cleaned_match_history.append(match)
+
+            if counter == 20:
+                break  
+
+        return cleaned_match_history 
+
     def getGameStats(self, matchId):
         """ Gets a game's stats given a matchId; matchId to be passed into the URL 
 
@@ -92,8 +122,6 @@ class MatchData:
         :type matchId: String
         :rtype: Dict
         """
-
-
         match_timeline_endpoint = "/lol/match/v4/timelines/by-match/"
         final_url = self.api + match_timeline_endpoint + matchId 
 
@@ -102,7 +130,7 @@ class MatchData:
 
         return curr_match_timeline 
 
-     def getTotalTimeline(self, match_history):
+    def getTotalGameTimeline(self, match_history):
         """ Returns a total timeline for each match played in match_history. 
 
         :type match_history: List[dict] 
@@ -118,72 +146,39 @@ class MatchData:
             
         return total_timeline 
 
-
-    def cleanMatchHistory(self, match_history):
-        """ Cleans match history by removing all games that are not on Summoner's Rift (i.e. removing games that are all aram)
-
-        :type match_history: List[Dict]
-        :rtype: List[Dict] 
-        """  
-
-        cleaned_match_history = [] 
-        counter = 0
-        for match in match_history:
-            matchId = str(match['gameId'])
-            curr_match_stats = self.getGameStats(matchId)  
-            curr_game_mode = curr_match_stats['gameMode']
-
-            print(matchId) 
-            print(curr_game_mode)
-            print("Counter: " + str(counter) + "\n")
-            counter += 1
-            time.sleep(6) 
-
-            if curr_game_mode != "CLASSIC":
-                continue 
-            else: 
-                cleaned_match_history.append(match) 
-
-        return cleaned_match_history 
-
-    def blueWins(self, match_history):
+    def blueWins(self, total_game_stats):
         """
-        Counts the occurrences at which blue wins given total match history. The output is 1 if the blue team wins, 0 if 
-        the blue team loses. (Note, pass in a clean match history, as we don't care about ARAM related games).
+        Counts the occurrences at which blue wins given a list of game stats for each game. The output is 1 if the blue team wins, 0 if 
+        the blue team loses.
 
-        :type match_history: List[dict]
+        :type total_game_stats: List[dict]
         :rtype: List[bool]
         """
 
         blueWins = [] 
 
-        for match in match_history:
-            matchId = str(match['gameId'])
-            curr_match_stats = self.getGameStats(matchId)  
-            teams = curr_match_stats["teams"]
+        for game in total_game_stats:
+            teams = game["teams"]
             blue_team = teams[0] # Returns a dict of the blue team's basic stats, with winning or losing indicated
             if blue_team["win"] == "Win":
                 blueWins.append(1) 
             else:
-                blueWins.append(0) 
+                blueWins.append(0)  
 
         return blueWins 
 
-    def redWins(self, match_history):
+    def redWins(self, total_game_stats):
         """
-        Counts the occurrences at which the red team wins given total match history. The output is 1 if the red team wins, 0 if the
-        red team loses. (Note, pass in a clean match history,as we don't care about ARAm related games).
+        Counts the occurrences at which the red team wins given a list of game stats for each game. The output is 1 if the red team wins, 0 if the red team loses.
 
-        :type match_history: List[dict]
+        :type total_game_stats: List[dict]
         :rtype: List[bool]
         """
 
         redWins = [] 
 
-        for match in match_history:
-            matchId = str(match['gameId'])
-            curr_match_stats = self.getGameStats(matchId)  
-            teams = curr_match_stats["teams"]
+        for game in total_game_stats :
+            teams = game["teams"]
             red_team = teams[1] # Returns a dict of the red team's basic stats, with winning or losing indicated
             if red_team["win"] == "Win":
                 redWins.append(1) 
@@ -193,27 +188,24 @@ class MatchData:
         return redWins 
 
 
-    def blueWardsPlaced(self, match_history):
+    def blueWardsPlaced(self, total_game_timeline):
         """
-        Counts the number of wards the blue team had placed down by the 15 minute mark for each match in match history. 
+        Counts the number of wards the blue team had placed down by the 15 minute mark for each match in the total game timeline. 
 
 
-        :type match_history: List[dict]
+        :type total_game_timeline: List[dict]
         :rtype: List[int] 
         """
 
         blueWardsPlaced = [] 
 
-        for match in match_history:
-            
+        for timeline in total_game_timeline:
             numBlueWards = 0 
-            matchId = str(match['gameId'])
-            curr_match_timeline = self.getGameTimeline(matchId) 
 
-            frames = curr_match_timeline["frames"]
+            frames = timeline["frames"]
 
             # Note that we loop 15 times, as we care only about the first 15 minutes of the game
-            for i in range(len(15)): 
+            for i in range(16): 
                 # This gives us the current frame that we're on (i.e. the ith to ith + 1 minute of the game)
                 curr_frame = frames[i] 
                 # Gives us an array of the events that took place during this ith minute 
@@ -222,13 +214,15 @@ class MatchData:
                 for j in range(len(curr_events)):
                     # Indexing into curr_events gives a dict of an event 
                     event = curr_events[j] 
-                    if event["type"] == "WARD_PLACED" and event["participantId"] < 6:
+                    # NOTE: We only care about blue wards, yellow wards, and pink wards 
+                    if event["type"] == "WARD_PLACED" and event["wardType"] != "UNDEFINED" and event["creatorId"] < 6:
                         numBlueWards += 1
+
             blueWardsPlaced.append(numBlueWards)
-        
+
         return blueWardsPlaced
 
-    def redWardsPlaced(self, match_history):
+    def redWardsPlaced(self, total_game_timeline):
         """
         Counts the number of wards the red team had placed down by the 15 minute mark for each match in match history. 
 
@@ -239,16 +233,13 @@ class MatchData:
 
         redWardsPlaced = [] 
 
-        for match in match_history:
-            
+        for timeline in total_game_timeline:
             numRedWards = 0 
-            matchId = str(match['gameId'])
-            curr_match_timeline = self.getGameTimeline(matchId) 
 
-            frames = curr_match_timeline["frames"]
+            frames = timeline["frames"]
 
             # Note that we loop 15 times, as we care only about the first 15 minutes of the game
-            for i in range(len(15)): 
+            for i in range(16): 
                 # This gives us the current frame that we're on (i.e. the ith to ith + 1 minute of the game)
                 curr_frame = frames[i] 
                 # Gives us an array of the events that took place during this ith minute 
@@ -257,32 +248,30 @@ class MatchData:
                 for j in range(len(curr_events)):
                     # Indexing into curr_events gives a dict of an event 
                     event = curr_events[j] 
-                    # Checks that the event type is a ward placement 
-                    # and the particiant is on the red team 
-                    if event["type"] == "WARD_PLACED" and event["participantId"] > 5:
+                    # NOTE: We only care about blue wards, yellow wards, and pink wards 
+                    if event["type"] == "WARD_PLACED" and event["wardType"] != "UNDEFINED" and event["creatorId"] > 5:
                         numRedWards += 1
+
             redWardsPlaced.append(numRedWards)
-        
+
         return redWardsPlaced
     
-    def blueWardsDestroyed(self, match_history):
+    def blueWardsDestroyed(self, total_game_timeline):
         """
-        Counts the number of wards the blue team had destroyed by the 15 minute mark for each match in match history. 
+        Counts the number of wards the blue team had destroyed by the 15 minute mark for each match in total_game_timeline. 
 
-        :type match_history: List[dict]
+        :type total_game_timeline: List[dict]
         :rtype: List[int] 
         """
 
         blueWardsDestroyed = []
 
-        for match in match_history:
+        for timeline in total_game_timeline:
             numDestroyed = 0
-            matchId = str(match['gameId'])
-            curr_match_timeline = self.getGameTimeline(matchId)
-            frames = curr_match_timeline["frames"]
+            frames = timeline["frames"]
 
             # Note that we loop 15 times, as we care only about the first 15 minutes of the game
-            for i in range(len(15)): 
+            for i in range(16): 
                 # This gives us the current frame that we're on (i.e. the ith to ith + 1 minute of the game)
                 curr_frame = frames[i] 
                 # Gives us an array of the events that took place during this ith minute 
@@ -294,29 +283,29 @@ class MatchData:
                     # Checks that the event type is a ward placement 
                     # and the particiant is on the red team 
                     if event["type"] == "WARD_KILL" and event["killerId"] < 6:
-                        numDestroyed += 1
-
+                        if event["wardType"] == "YELLOW_TRINKET" or event["wardType"] == "CONTROL_WARD":
+                            numDestroyed += 1
+           
             blueWardsDestroyed.append(numDestroyed) 
         return blueWardsDestroyed
     
-    def redWardsDestroyed(self, match_history):
+    def redWardsDestroyed(self, total_game_timeline):
         """ 
-        Counts the number of wards the red team had destroyed by the 15 minute mark for each match in match history. 
+        Counts the number of wards the red team had destroyed by the 15 minute mark for each match in total_game_timeline. 
 
-        :type match_history: List[dict]
+        :type total_game_timeline: List[dict]
         :rtype: List[int] 
         """
 
         redWardsDestroyed = []
 
-        for match in match_history:
+        for timeline in total_game_timeline:
             numDestroyed = 0
-            matchId = str(match['gameId'])
-            curr_match_timeline = self.getGameTimeline(matchId)
-            frames = curr_match_timeline["frames"]
+            
+            frames = timeline["frames"]
 
             # Note that we loop 15 times, as we care only about the first 15 minutes of the game
-            for i in range(len(15)): 
+            for i in range(16): 
                 # This gives us the current frame that we're on (i.e. the ith to ith + 1 minute of the game)
                 curr_frame = frames[i] 
                 # Gives us an array of the events that took place during this ith minute 
@@ -328,7 +317,8 @@ class MatchData:
                     # Checks that the event type is a ward placement 
                     # and the particiant is on the red team 
                     if event["type"] == "WARD_KILL" and event["killerId"] > 5:
-                        numDestroyed += 1
+                        if event["wardType"] == "YELLOW_TRINKET" or event["wardType"] == "CONTROL_WARD":
+                            numDestroyed += 1
 
             redWardsDestroyed.append(numDestroyed) 
         return redWardsDestroyed
@@ -663,5 +653,9 @@ if __name__ == "__main__":
     accountId = data.getAccountID(summoner_name) 
     match_history = data.getTotalMatchHistory(accountId)
     cleaned_match_history = data.cleanMatchHistory(match_history)
-    pprint.pprint(cleaned_match_history)
-    print("Cleaned Match History Size: " + str(len(cleaned_match_history)))
+    # total_game_stats = data.getTotalGameStats(cleaned_match_history)
+    total_game_timeline = data.getTotalGameTimeline(cleaned_match_history)
+
+    blueWardsDestroyed = data.blueWardsDestroyed(total_game_timeline) 
+
+    print(blueWardsDestroyed)     
