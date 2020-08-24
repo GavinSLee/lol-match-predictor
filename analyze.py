41c31e9c4554315257157821749fbce2506613bf
Gavin Lee <46236119+GavinSLee@users.noreply.github.com>
2020-08-24 15:06:09 -0700

    More and more

--- analyze.py
+++ analyze.py
@@ -5,13 +5,8 @@ import seaborn as sns
 
 df = pd.read_csv('league_data.csv')
 df_clean = df.copy() 
-df_clean = df_clean.drop(['gameId'], axis = 1)
+cols = ['gameId', 'redFirstBlood', 'redKills', 'redEliteMonsters', 'redDragons','redTotalMinionsKilled',
+       'redTotalJungleMinionsKilled', 'redGoldDiff', 'redExperienceDiff', 'redCSPerMin', 'redGoldPerMin', 'redHeralds',
+       'blueGoldDiff', 'blueExperienceDiff', 'blueCSPerMin', 'blueGoldPerMin', 'blueTotalMinionsKilled']
 
-g = sns.PairGrid(data=df_clean, vars=['blueKills', 'blueAssists', 'blueWardsPlaced', 'blueTotalGold'], hue='blueWins', height=3, palette='Set1')
-g.map_diag(plt.hist)
-g.map_offdiag(plt.scatter)
-g.add_legend();
-plt.figure(figsize=(16, 12))
-sns.heatmap(df_clean.drop('blueWins', axis = 1).corr(), cmap="YlGnBu", annot = True, fmt = '.2f', vmin = 0)
-
-plt.show() 
+df_clean.info() 
